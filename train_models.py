14d273d60fc42de2e850bb83c986207864669e16
w. Patrick Gale <w.patrick.gale@unc.edu>
2023-03-28 15:04:59 -0400

    model training test

--- train_models.py
+++ train_models.py
@@ -598,9 +598,9 @@ class TransferLearningPytorchModel():
             if self.n_additional_image_features_to_predict > 0:
                 additional_feature_outputs = outputs[:, -self.n_additional_image_features_to_predict:]
                 outputs = outputs[:, :-self.n_additional_image_features_to_predict]
-                print("additional_feature_outputs on self.n_additional_image_features_to_predict > 0=",str(additional_feature_outputs)) # debug
-                print("outputs on self.n_additional_image_features_to_predict > 0=",str(outputs)) # debug
-                print("labels on self.n_additional_image_features_to_predict > 0=",str(labels)) # debug
+                # print("additional_feature_outputs on self.n_additional_image_features_to_predict > 0=",str(additional_feature_outputs)) # debug
+                # print("outputs on self.n_additional_image_features_to_predict > 0=",str(outputs)) # debug
+                # print("labels on self.n_additional_image_features_to_predict > 0=",str(labels)) # debug
                 loss = self.loss_criterion(input=outputs, target=labels) 
 
                 # basically, we only add to the additional feature loss if a feature is not NaN.  
